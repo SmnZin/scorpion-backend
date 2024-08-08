95d0def3bab31fad70152dc65fb2ebf8ada65313
SmnZin <scarrascoi@utem.cl>
2024-08-07 21:23:10 -0400

    mostrar error en index.js

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ require('./src/database');
 // esta logica es para ejecutra el servidor
 
 async function main() {
-    await app.listen(app.get('port'));
-    console.log('Server se esta ejecutando en el puerto: ', app.get('port'));
+    try {
+        await app.listen(app.get('port'));
+        console.log('Server se esta ejecutando en el puerto: ', app.get('port'));
+    } catch (error) {
+        console.error('Error al iniciar el servidor:', error);
+        process.exit(1); // Termina el proceso con un código de error
+    }
 }
+
 
 main();
 
